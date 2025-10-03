4b1caf40d6cc4f0b4d70533742a3c2e50415aea8
doginvi <victordesousaestudo@gmail.com>
2025-10-03 08:40:08 -0300

    FIX: Criação de const para o create app

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).mount('#app');
+const app = createApp(App);
 app.use(router);
 
 app.mount('#app');
