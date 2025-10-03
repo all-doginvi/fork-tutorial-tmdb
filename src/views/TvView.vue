0028d74e527cde49c807da92fe7dafdc65b21d78
doginvi <victordesousaestudo@gmail.com>
2025-10-03 09:20:41 -0300

    FEAT: Create TV Show Genres #9

---
+++ src/views/TvView.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+    import { ref, onMounted } from 'vue';
+    import api from '@/plugins/axios'
+
+    const genres = ref([]);
+
+    onMounted( async () => {
+        const response = await api.get('genre/tv/list?language=pt-BR');
+        genres.value = response.data.genres;
+    });
+
+</script>
+
+<template>
+    <div>
+        <h1>Gêneros dos Programas de TV</h1>
+    </div>
+    <ul>
+        <li v-for="genre in genres" :key="genre.id"> {{ genre.name }} </li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
