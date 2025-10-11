f25474fc2cf5d273d43d861b4447d8df7f3e71e8
doginvi <victordesousaestudo@gmail.com>
2025-10-11 14:40:08 -0300

    FEAT: Alterando o store de gêneros

--- src/stores/genre.js
+++ src/stores/genre.js
@@ -5,7 +5,14 @@ import api from '@/plugins/axios'
 export const useGenreStore = defineStore('genre', () => {
     const state = reactive({
         genres: [],
+        currentGenreId: null,
     });
+
+    const currentGenreId = computed(() => state.currentGenreId);
+
+    const setCurrentGenreId = (genreId) => {
+        state.currentGenreId = genreId;
+    };
 
     const genres = computed(() => state.genres);
     const getGenreName = (id) => state.genres.find((genre) => genre.id === id).name;
@@ -15,5 +22,5 @@ export const useGenreStore = defineStore('genre', () => {
         state.genres = response.data.genres;
     };
 
-    return { genres, getAllGenres, getGenreName };
+    return { genres, getAllGenres, getGenreName, currentGenreId, setCurrentGenreId };
 } )
