35175984798ce0f3c0a96d9e45f8e43251f79461
victor-de-sousa <victordesousaestudo@gmail.com>
2025-11-02 16:09:25 -0300

    feat: produtoras-brasileiras

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,25 @@
 <script setup>
+
+import api from '@/plugins/axios';
+import { ref, onMounted } from 'vue';
+
+const companies = ref([]);
+
+onMounted(async () => {
+  for (let indice = 7000; indice < 100000; indice++) {
+    try {
+      const response = await api.get(`company/${indice}`);
+      const company = response.data;
+
+      if (company.origin_country === 'BR') {
+        companies.value.push(company);
+      }
+    } catch (error) {
+      console.log(`Empresa ${indice} inexistente`);
+    };
+  };
+});
+
 </script>
 
 <template>
@@ -14,29 +35,39 @@
   <main>
     <router-view />
   </main>
+
+  <main>
+    <ul>
+      <li v-for="company in companies" :key="company.id">
+        {{ company.id }} -
+        {{ company.name }}
+        {{ company.origin_country }}
+      </li>
+    </ul>
+  </main>
+
+
 </template>
 
 <style scoped>
+header {
+  height: 3rem;
+  display: flex;
+  background-color: black;
+  color: #fff;
+  font-size: 1.2rem;
+  padding-left: 2rem;
+}
 
-  header {
-    height: 3rem;
-    display: flex;
-    background-color: black;
-    color: #fff;
-    font-size: 1.2rem;
-    padding-left: 2rem;
-  }
+nav {
+  column-gap: 2rem;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
+}
 
-  nav {
-    column-gap: 2rem;
-    margin-bottom: 0;
-    display: flex;
-    align-items: center;
-  }
-
-  nav a {
-    text-decoration: none;
-    color: #fff;
-  }
-
+nav a {
+  text-decoration: none;
+  color: #fff;
+}
 </style>
